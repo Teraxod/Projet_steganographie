2eb060bf2faaaffdc2d2c1aeef135ad1d32cbc02
Teraxod <teraxod@gmail.com>
2025-01-30 11:10:29 +0100

    Update and rename steganographie.py to cache_exif.py

--- cache_exif.py
+++ cache_exif.py
@@ -1,8 +1,11 @@
+# Ce script va cacher le flag dans la partie exif de l'image
+
+
 from PIL import Image
 import piexif
 
 # Ouvre l'image
-image_path = 'C:/Users/r.voisin/Documents/image.jpg'
+image_path = 'C:/Users/Documents/image.jpg'
 image = Image.open(image_path)
 
 # Charge les métadonnées EXIF existantes
@@ -12,7 +15,7 @@ exif_dict = piexif.load(image.info.get("exif"))
 exif_dict['0th'][piexif.ImageIFD.ImageDescription] = "FLag(cyber_T4er_flag_8)"
 
 # Sauvegarde l'image avec les métadonnées EXIF modifiées
-output_path = 'C:/Users/r.voisin/Documents/image_text_exif.jpg'
+output_path = 'C:/Users/Documents/image_text_exif.jpg'
 image.save(output_path, exif=piexif.dump(exif_dict))
 
 print(f"Le texte a été ajouté aux métadonnées EXIF de l'image. Nouvelle image enregistrée sous {output_path}")
