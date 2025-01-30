de6cbdce3ec725ebd2f95735b7c9df49d6f50839
Teraxod <teraxod@gmail.com>
2025-01-30 11:46:15 +0100

    Update cache_exif.py

--- cache_exif.py
+++ cache_exif.py
@@ -4,17 +4,13 @@
 from PIL import Image
 import piexif
 
-# Ouvre l'image
 image_path = 'C:/Users/Documents/image.jpg'
 image = Image.open(image_path)
 
-# Charge les métadonnées EXIF existantes
 exif_dict = piexif.load(image.info.get("exif"))
 
-# Ajoute un texte personnalisé dans le champ ImageDescription
 exif_dict['0th'][piexif.ImageIFD.ImageDescription] = "FLag(cyber_T4er_flag_8)"
 
-# Sauvegarde l'image avec les métadonnées EXIF modifiées
 output_path = 'C:/Users/Documents/image_text_exif.jpg'
 image.save(output_path, exif=piexif.dump(exif_dict))
 
