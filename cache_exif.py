3d0028f98db96b1ff5f785365d1610ffb8b6d8d5
mwntns <129161106+mwntns@users.noreply.github.com>
2025-01-31 23:04:48 +0100

    Update cache_exif.py

--- cache_exif.py
+++ cache_exif.py
@@ -5,17 +5,22 @@ from PIL import Image
 import piexif
 
 # Ouvre l'image
-image_path = 'C:/Users/Documents/image.jpg'
+image_path = 'image.jpg'
 image = Image.open(image_path)
 
-# Charge les métadonnées EXIF existantes
-exif_dict = piexif.load(image.info.get("exif"))
+# Vérifie si l'image a des métadonnées EXIF, sinon initialise un dictionnaire vide
+exif_bytes = image.info.get("exif")
+if exif_bytes:
+    exif_dict = piexif.load(exif_bytes)
+else:
+    exif_dict = {"0th": {}, "Exif": {}, "GPS": {}, "Interop": {}, "1st": {}, "thumbnail": None}
 
 # Ajoute un texte personnalisé dans le champ ImageDescription
-exif_dict['0th'][piexif.ImageIFD.ImageDescription] = "FLag(cyber_T4er_flag_8)"
+exif_dict['0th'][piexif.ImageIFD.ImageDescription] = b"FLag(test)"
 
 # Sauvegarde l'image avec les métadonnées EXIF modifiées
-output_path = 'C:/Users/Documents/image_text_exif.jpg'
-image.save(output_path, exif=piexif.dump(exif_dict))
+output_path = 'image_text_exif.jpg'
+image.save(output_path, "jpeg", exif=piexif.dump(exif_dict))
 
 print(f"Le texte a été ajouté aux métadonnées EXIF de l'image. Nouvelle image enregistrée sous {output_path}")
+
